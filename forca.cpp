f76efdae77212c482bd72e7dbcb19b9e9a690de8
diegoperesmarques <diegoperesmarques@hotmail.com>
2023-05-28 13:52:26 -0300

    Adicionando palavras na forca

--- forca.cpp
+++ forca.cpp
@@ -116,6 +116,40 @@ void sorteia_palavra() {
     palavra_secreta = palavras[indice_sorteio];
 }
 
+void salva_arquivo(vector<string> nova_lista) {
+    ofstream arquivo;
+    arquivo.open("palavras.txt");
+
+    if(arquivo.is_open()) 
+    {
+        arquivo << nova_lista.size() << endl;
+
+        for(string palavra : nova_lista) {
+
+        }
+
+        arquivo.close();
+    } else 
+    {
+        cout << "Não foi possível abrir o arquivo" << endl;
+        exit(0);
+    }
+
+}
+
+
+void adiciona_palavra() {
+    cout << "Digite a nova palavra usando letras maiúsculas" << endl;
+    string nova_palavra;
+    cin >> nova_palavra;
+
+    vector<string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+
+    salva_arquivo(lista_palavras);
+
+}
+
 int main () {
     imprime_cabecalho();
 
@@ -137,5 +171,12 @@ int main () {
     }
     else{
         cout << "Parabéns! Você acertou a palavra secreta!" << endl;
+        cout << "Você deseja adicionar uma nova palavra ao banco (S/N)? ";
+        char resposta;
+        cin >> resposta;
+
+        if(resposta == 'S') {
+            adiciona_palavra();
+        }
     }
 }
